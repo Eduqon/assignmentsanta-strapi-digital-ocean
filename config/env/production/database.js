909347cdb66a8702443743fe1bef23537f588ca3
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-10-27 14:13:25 +0530

    Managed Database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,9 +1,13 @@
 const parse = require("pg-connection-string").parse;
 
 const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
+  "postgresql://doadmin:AVNS_j-5W8uko5FP79Ws0_Ml@assignmentsanta-strapi-db-do-user-13305520-0.c.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
 );
-console.log({ url: process.env.DATABASE_URL });
+console.log({
+  url: process.env.DATABASE_URL,
+  newURL:
+    "postgresql://doadmin:AVNS_j-5W8uko5FP79Ws0_Ml@assignmentsanta-strapi-db-do-user-13305520-0.c.db.ondigitalocean.com:25060/defaultdb?sslmode=require",
+});
 
 module.exports = ({ env }) => ({
   connection: {
