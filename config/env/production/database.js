cfd3a77f6d99ee2328d1478cd6570c3c28d60df5
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-11-12 22:52:10 +0530

    update the directory and subdomain url for images

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,14 +3,6 @@ const parse = require("pg-connection-string").parse;
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
-console.log({
-  url: process.env.DATABASE_URL,
-  host,
-  port,
-  database,
-  user,
-  password,
-});
 
 module.exports = ({ env }) => ({
   connection: {
